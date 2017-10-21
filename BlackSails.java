485e66524cace572d69d72fe26a2ef0172779ea5
Abdul Alsaiari <d7mi-uk@hotmail.com>
2017-10-21 21:25:50 +0100

    Update BlackSails.java

--- BlackSails.java
+++ BlackSails.java
@@ -1,6 +1,6 @@
 Public Class BlackSails{
 public static void main (String []args){
 System.out.print("BlackSails");
-System.out.println("Peaky Bliners");
+//System.out.println("Peaky Bliners");
 		}
 	}
